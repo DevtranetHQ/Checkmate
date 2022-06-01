9c02ae9a144d63a0b87fa941de2a24dbc5f1e3b7
EsprIx <3sprix.exe@gmail.com>
2022-06-02 00:07:37 +0200

    changed request to add user

--- helpers/requests.py
+++ helpers/requests.py
@@ -1,9 +1,19 @@
 import requests as req
+import os
+import sys
 import json
+
+if not os.path.isfile("../config.json"):
+    sys.exit("'config.json' not found! Please add it and try again.")
+else:
+    with open("../config.json") as file:
+        config = json.load(file)
+
+headers = {"X-API-KEY": config["X-API-KEY"]}
 
 
 def email_in_endpoint(url, email) -> bool:
-    r = req.post(url, json={"email": email})
+    r = req.post(url, json={"email": email}, headers=headers)
     if r.status_code == 200:
         return True
     else:
@@ -11,7 +21,9 @@ def email_in_endpoint(url, email) -> bool:
 
 
 def add_user(url, email, discordId, discordUsername) -> bool:
-    r = req.post(url, json={"email": email, "discordId": discordId, "discordUsername": discordUsername})
+    r = req.post(
+        url, json={"email": email, "discordId": discordId, "discordUsername": discordUsername}, headers=headers
+    )
     if r.status_code == 201:
         return True
     else:
